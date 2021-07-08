27ddf265e0a81bf264bea0c6258fe071e56cd3e4
korede <egbeolowoakorede97@gmail.com>
2021-07-08 11:39:34 -0500

    Changes

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ app.use(function(req, res, next) {
   
     // render the error page
     res.status(err.status || 500);
-        res.json('errors');
+        res.render('errors');
     });
 
 app.listen(port, () => console.info(`Listening to port ${port}`));
